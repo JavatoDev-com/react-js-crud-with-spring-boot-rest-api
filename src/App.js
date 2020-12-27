1f3a2b4b03c92a80bcd736a3be9714dc1b369b62
javatodev <sendtospringboot@gmail.com>
2020-12-28 00:55:20 +0530

    router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          React JS CRUD Example With Spring Boot REST API
-        </p>
-      </header>
+    <div className="container">
+      <div className="row">
+      <h2>React JS CRUD Example With Spring Boot REST API</h2>
+      <Router>
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link to={'/'} className="navbar-brand">React CRUD Example</Link>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                  <Link to={'/'} className="nav-link">Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to={'/create'} className="nav-link">Create</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to={'/index'} className="nav-link">Index</Link>
+                </li>
+              </ul>
+            </div>
+          </nav> <br/>
+          <h2>Welcome to React CRUD Tutorial</h2> <br/>
+          <Switch>
+              <Route exact path='/create' component={ App } />
+              <Route path='/edit/:id' component={ App } />
+              <Route path='/index' component={ App } />
+          </Switch>
+        </div>
+      </Router>
+      </div>
     </div>
   );
 }
